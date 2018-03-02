c7f8c533042a958256a1b54e169057a048f0d58d
Jessica C <jgcreiglow@gmail.com>
2018-03-02 00:16:36 -0500

    omdb

--- liri.js
+++ liri.js
@@ -1,44 +1,78 @@
-require("dotenv").config();
 //Dependencies
+require("dotenv").config();
 var fs = require("fs");
 var inquirer = require("inquirer");
 var twitter = require("twitter");
 var Spotify = require("node-spotify-api");
+var request = require("request")
 var keys = require("./keys.js");
 
-//read keys from Twitter and Spotify 
-fs.readFile("./keys.js", "utf8", function(error, data) {
-    // var spotify = new (keys.spotify);
-    var spotify = new Spotify(keys.spotify);
-    var client = new twitter(keys.twitter);
-//Twitter API Work
-if(process.argv[2] === "my-tweets"){
-    client.get('search/tweets', {q: '@jscagrcecrglw', count: 20}, function(error, data) {
-        if(!error) {
-            console.log(data)
-        }
-        else {
-            console.log ("sorry, there's an error")
-        }
+//Switch to run the different functions
 
-      });
+switch (process.argv[2]) {
+    
+// Twitter API Work
+    case "my-tweets":
+        return fs.readFile("./keys.js", "utf8", function(error, data) {
 
-    }
+            var client = new twitter(keys.twitter);
+            var params = {
+                jscagrcecrglw: 'nodejs'
+            };
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+                if (!error) {
+                    console.log(text);
+                }
+            });
+        });
 
-if (process.argv[2] === "spotify-this-song"){
 //Spotify API Work
-    spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-   
-    if(!error) {
-        console.log('Artist: ' + data.tracks.items[0].artists[0].name);
-        console.log('Title: ' + data.tracks.items[0].name);
-        console.log('Album: ' + data.tracks.items[0].album.name);
-        console.log('Preview link: ' + data.tracks.items[0].preview_url);
-    }
+    case "spotify-this-song":
+        return fs.readFile("./keys.js", "utf8", function(error, data) {
+            var spotify = new Spotify(keys.spotify);
+            spotify.search({
+                type: 'track',
+                query: process.argv[3]
+            }, function(err, data) {
 
-    else{
-        console.log("I don't recognize that song")
-    }
-    });
-};
-});
+                if (!error) {
+                    console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+                    console.log('Title: ' + data.tracks.items[0].name);
+                    console.log('Album: ' + data.tracks.items[0].album.name);
+                    console.log('Preview link: ' + data.tracks.items[0].preview_url);
+                } else {
+                    console.log("I don't recognize that song")
+                }
+            });
+        });
+//OMDb API Work
+    case "movie-this":
+        var Args = process.argv;
+
+        var movieName = "";
+
+        for (var i = 3; i < Args.length; i++) {
+            if (i > 3 && i < Args.length) {
+                movieName = movieName + "+" + Args[i];
+            } else {
+                movieName += Args[i];
+            }
+        }
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=t&apikey=trilogy";
+
+
+        console.log(queryUrl);
+
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country of Production: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+            }
+        });
+}
